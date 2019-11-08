3b336d1b6851eafcc4f4f69a6caf424029b82c82
maoap1 <maoap1@gmail.com>
2019-11-09 00:30:03 +0100

    Movement

--- WeaponOfChoice/Assets/PlayerController.cs
+++ WeaponOfChoice/Assets/PlayerController.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-
+    public float speed = 200f;
+    public float jumpSpeed = 200f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
         
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal0"), 0, 0);
+        transform.position += movement * speed * Time.fixedDeltaTime;
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(jumpSpeed*Input.GetAxis("Jump0")*Vector2.up);
     }
 }
