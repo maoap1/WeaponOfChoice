f9ebdf0bb28dd04509abdc4b108785ef2be6a4ca
cchudac <cchudac@gmail.com>
2019-11-09 00:28:44 +0100

    BaseOfWeapons, Weapon and WeponsTypes added.

---
+++ WeaponOfChoice/Assets/Weapon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Weapon : MonoBehaviour
+{
+	public PlayerController pc { set; protected get; }
+	public float reloadTime = 0.8f;
+	public int attackStrength = 10;
+	public abstract void Attack();
+}
+public abstract class RangedWeapon : Weapon
+{
+	public GameObject Projectile;
+	public override void Attack()
+	{
+		throw new System.NotImplementedException();
+	}
+}
+public abstract class MeleeWeapon : Weapon
+{
+}
+public sealed class Fork : MeleeWeapon
+{
+	public override void Attack()
+	{
+		throw new System.NotImplementedException();
+	}
+}
+public sealed class Toaster : RangedWeapon
+{
+}
+
