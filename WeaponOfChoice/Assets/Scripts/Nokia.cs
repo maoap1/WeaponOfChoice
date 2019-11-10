622abba37b77f5c77228062fa0203e3b8184eceb
maoap1 <maoap1@gmail.com>
2019-11-10 12:21:45 +0100

    Fading of scenes now works

--- WeaponOfChoice/Assets/Scripts/Nokia.cs
+++ WeaponOfChoice/Assets/Scripts/Nokia.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public sealed class Nokia : Weapon
 {
-
     protected override bool Melee => true;
 
-    public Nokia()
-    {
-        attackStrength = 49;
-        ProjectileDiesAfter = 1.80347f;
-        projectileSpeed = 7;
-        ProjectileGravityScale = -0.14f;
-        reloadTime = 2.5f;
-    }
 }
