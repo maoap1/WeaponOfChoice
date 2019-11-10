0d3961531bdac5822e26623ca9f8b793ea3e19e7
cchudac <cchudac@gmail.com>
2019-11-10 15:32:00 +0100

    counter

--- WeaponOfChoice/Assets/Scripts/SecondsCounter.cs
+++ WeaponOfChoice/Assets/Scripts/SecondsCounter.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class SecondsCounter : MonoBehaviour
 {
-	List<GameObject> counterMarks;
+	public List<GameObject> counterMarks;
 
 	GameObject instantiated;
-
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
